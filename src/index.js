0b957221f671b6040880baf49c0a1682c5f582cb
DmiE <Dawid.Mieszczak95@gmail.com>
2018-10-30 17:19:10 +0100

    add UNDO function

--- src/index.js
+++ src/index.js
@@ -78,11 +78,12 @@ import './index.css';
       super(props);
       this.state = {
         history: [{squares: Array(9).fill(null)}], 
-        xIsNext: true};
+        xIsNext: true,
+        stepNumber: 0};
     }
 
     handleClick = (i) => {
-      const history = this.state.history;
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
       const current = history[history.length - 1];
       const squares = current.squares.slice();    // thanks to slice method we create new array instead of mutating the old one.
       if (calculateWinner(squares) || squares[i]) {
@@ -97,13 +98,32 @@ import './index.css';
       this.setState({
         history : history.concat([{
           squares: squares}]),
+        stepNumber: history.length,
         xIsNext: !this.state.xIsNext});
+    }
+
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
+      });
     }
     
     render() {
       const history = this.state.history;
-      const current = history[history.length - 1];
+      const current = history[this.state.stepNumber];
       const winner = calculateWinner(current.squares);
+      
+      const moves = history.map((step, move) => {
+        const desc = move ? 
+          'Go to move #' + move :
+          'Go to game start';
+        return (
+          <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        )
+      })
 
       let status
       
@@ -121,7 +141,7 @@ import './index.css';
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
