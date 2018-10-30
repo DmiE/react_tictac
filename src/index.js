ebf1cd1f0ab9d053bc6199c7314b437e06f85ea7
DmiE <Dawid.Mieszczak95@gmail.com>
2018-10-30 12:04:51 +0100

    add function -calculateWinner- and information, who won the game

--- src/index.js
+++ src/index.js
@@ -21,6 +21,26 @@ import './index.css';
       </button>
     );
   }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   
   class Board extends React.Component {
     constructor(props) {
@@ -30,6 +50,9 @@ import './index.css';
 
     handleClick = (i) => {
       const squares = this.state.squares
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
       if (this.state.xIsNext == true) {
         squares[i] = "X"
       }
@@ -47,8 +70,14 @@ import './index.css';
     }
   
     render() {
-      const status = 'Next player: X';
-  
+      const winner = calculateWinner(this.state.squares)
+      let status
+      if (winner) {
+        status = "Player " + (winner) + " won!"
+      }
+      else {
+        status = 'Next player: ' + (this.state.xIsNext ? "X" : "O");
+      }
       return (
         <div>
           <div className="status">{status}</div>
